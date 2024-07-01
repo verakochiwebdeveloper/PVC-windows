352fc0d13136dc1d249107b08e20916e777cbc16
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-07-01 07:59:07 +0300

    index js modify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import menu from './modules/menu';
-import modal from './modules/modal';
-import tabs from './modules/tabs';
-import slider from './modules/slider';
-import calc from './modules/calc';
-import sendForm from './modules/sendForm';
+import menu from './modules/popup';
+
 
