8c2291ce20c1505ee759aa915c998bad98c10659
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-07-01 07:55:35 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -5,32 +5,3 @@ import slider from './modules/slider';
 import calc from './modules/calc';
 import sendForm from './modules/sendForm';
 
-menu()
-modal()
-tabs()
-slider()
-calc(100)
-sendForm({
-    formId:'form1',
-     someElem:[
-        {
-            type: 'block',
-            id: 'total'
-        }
-     ]})
-     sendForm({
-        formId:'form2',
-         someElem:[
-            {
-                type: 'block',
-                id: 'total'
-            }
-         ]})
-         sendForm({
-            formId:'form3',
-             someElem:[
-                {
-                    type: 'block',
-                    id: 'total'
-                }
-             ]})
